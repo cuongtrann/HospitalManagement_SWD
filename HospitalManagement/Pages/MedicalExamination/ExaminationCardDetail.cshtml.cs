8408239e81e887015a23a8536d28586de721d0bb
vtrungkin13 <vtrungkin13@gmail.com>
2023-07-17 21:23:58 +0700

    Update ExaminationCardDetail.cshtml.cs

--- HospitalManagement/Pages/MedicalExamination/ExaminationCardDetail.cshtml.cs
+++ HospitalManagement/Pages/MedicalExamination/ExaminationCardDetail.cshtml.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-<<<<<<<< HEAD:HospitalManagement/Pages/MedicalExamination/ExaminationCardDetail.cshtml.cs
 namespace HospitalManagement.Pages.MedicalExamination
-========
-namespace HospitalManagement.Pages.Recieptionist
->>>>>>>> 12d1d91 (rebase branch):HospitalManagement/Pages/Recieptionist/ViewAppointment.cshtml.cs
 {
-    public class ViewAppointmentModel : PageModel
+    public class ExaminationCardDetailModel : PageModel
     {
         public void OnGet()
         {
